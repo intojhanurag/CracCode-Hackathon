48347cfada899119119c7f57dc800664836136da
Anurag Ojha <aojharaj2004@gmail.com>
2025-05-07 16:54:51 +0530

    path issue

--- middleware.ts
+++ middleware.ts
@@ -3,15 +3,17 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 // Define routes that should be protected
-const isProtectedRoute = createRouteMatcher([
-  "",  // Add the route you want to protect
-      // Add more routes as needed
-]);
+
+
+const isProtectedRoute = createRouteMatcher(["/(.*)"]); // matches all routes
+const isPublicRoute = createRouteMatcher(["/"]);  
+
+
 
 export default clerkMiddleware(async(auth, req) => {
   const {userId}=await auth();
-  if (isProtectedRoute(req)&&!userId) {
-    return new Response("Unauthorized",{status:401});
+  if (isProtectedRoute(req)&&!isPublicRoute(req)&&!userId) {
+    return NextResponse.redirect(new URL("/sign-in",req.url));
   }
 
   return NextResponse.next();
